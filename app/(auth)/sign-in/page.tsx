36f8c484c9f82ece47058547bbf784160941f229
Madeline <grammaticomadeline@hotmail.fr>
2025-02-05 22:53:21 +0100

    B01 : AUTH : add Sign in

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/src/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+import Link from "next/link";
+
+const Page = async () => {
+  return (
+    <div className="w-full max-w-sm mx-auto space-y-6 text-white">
+      <h1 className="text-2xl font-bold text-center mb-6">Se connecter</h1>
+        <p>Connection par Google bientôt!</p>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-sm">
+          <p className="bg-background px-2 text-muted-foreground rounded-sm">
+            Connection par mail
+          </p>
+        </div>
+      </div>
+
+      {/* Email/Password Sign In */}
+      <form
+        className="space-y-4"
+        action={async () => {
+          "use server";
+        }}
+      >
+        <Input
+          name="email"
+          placeholder="Email"
+          type="email"
+          required
+          autoComplete="email"
+        />
+        <Input
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+          autoComplete="current-password"
+        />
+        <Button className="w-full" type="submit">
+          Se connecter
+        </Button>
+      </form>
+
+      <div className="text-center">
+        <Button asChild variant="link">
+          <Link href="/sign-up">Pas encore de compte? Sign up</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
