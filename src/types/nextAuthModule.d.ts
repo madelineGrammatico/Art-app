e4c393226b7eafef33f02c7b12818e5c9233d605
Madeline <grammaticomadeline@hotmail.fr>
2025-02-20 16:41:45 +0100

    B01 : AUTH : rename nextAuthModule.d.ts

--- src/types/nextAuthModule.d.ts
+++ src/types/nextAuthModule.d.ts
@@ -1,17 +1,21 @@
+import { UserRole } from "@prisma/client"
 import type { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
   interface User {
     firstName?: string | null
     lastName?: string | null
+    role: UserRole
   }
+  
 
   interface Session extends DefaultSession{
     user: User & DefaultSession["user"]
   }
-
+  
   interface JWT {
     firstName?: string | null
     lastName?: string | null
+    role: UserRole
   }
 }
