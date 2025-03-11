e6b130b07615cbe06565c0363d39cc32d3fe7fbb
Madeline <grammaticomadeline@hotmail.fr>
2025-03-11 10:12:14 +0100

    B03 : ARTWORKS : sign in : refacto SignInAction

--- app/(auth)/sign-in/SignInAction.ts
+++ app/(auth)/sign-in/SignInAction.ts
@@ -4,9 +4,16 @@ import { signIn } from "@/src/lib/auth/auth";
 import { executeAction } from "@/src/lib/executeAction";
 
 export const CredentialsSignInAction = async(formData: FormData)=> {
+    const email = String(formData.get("email"))
+    const password = String(formData.get("password"))
+    if (!email || !password) throw new Error("Email ou password manquant")
+
     await executeAction({
         actionFn: async () => {
-            await signIn("credentials", formData)
+            await signIn("credentials", {email,
+                password,
+                redirect: false
+            })
         }
     })
 }
