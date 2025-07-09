939cfdda56b907d5c78a2d5b5336a240f3cb8717
Madeline <grammaticomadeline@hotmail.fr>
2025-07-09 20:46:59 +0200

    B04 : SESSION : remove tokens

--- src/types/nextAuthModule.d.ts
+++ src/types/nextAuthModule.d.ts
@@ -7,23 +7,13 @@ declare module "next-auth" {
     firstName?: string | null
     lastName?: string | null
     role: UserRole
-    // accessToken?: string
-    // refreshToken?: string
-    // accessTokenExpires?: number
     password?: string | null
   } 
   
   interface Session{
     user: User & DefaultSession["user"]
-    // accessToken: string
-    // accessTokenExpires: number
-    // error?: string
-    // sessionToken: string
-  }
-}
+    sessionToken: string
 
-// declare module "next-auth/jwt" {
-//   interface JWT extends Partial<Pick<User,"refreshToken"| "accessToken" |"accessTokenExpires"| "id"| "email"| "role">> {
-//   error?: string
-//   } 
-// }
+    // TO DO: add prisma session
+  }
+}   
