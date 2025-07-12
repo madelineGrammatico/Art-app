87ae3aab7d1765172405176be75d941c1ac77a6f
Madeline <grammaticomadeline@hotmail.fr>
2025-07-12 12:56:35 +0200

    B04 : SESSION : auth : refacto auth module

--- src/types/nextAuthModule.d.ts
+++ src/types/nextAuthModule.d.ts
@@ -1,19 +1,25 @@
 import { UserRole } from "@prisma/client"
-import type { DefaultSession } from "next-auth"
-
+ 
 declare module "next-auth" {
-  interface User
-  {
+  interface User  {
+    id: string
     firstName?: string | null
     lastName?: string | null
     role: UserRole
     password?: string | null
+    emailVerified?: Date | null
+    createdAt?: Date
+    updatedAt?: Date 
+    email?: string | null
+    image?: string | null 
   } 
-  
-  interface Session{
-    user: User & DefaultSession["user"]
-    sessionToken: string
 
-    // TO DO: add prisma session
+  interface Session {
+    user: User
+    id: string  | null
+    userId: string
+    sessionToken: string  | null
+    createdAt: Date 
+    updatedAt: Date
   }
-}   
+}
