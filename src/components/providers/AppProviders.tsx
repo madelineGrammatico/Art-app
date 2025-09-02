537aac5f8b72d1a8a7f1a513fd4147e146805be5
Madeline <grammaticomadeline@hotmail.fr>
2025-09-02 16:54:46 +0200

    B08 : TANSTACK : config clientQuery

--- src/components/providers/AppProviders.tsx
+++ src/components/providers/AppProviders.tsx
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 import { store } from '@/store/store';
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@/src/lib/tanStack/queryClient';
+import { getQueryClient } from '@/src/lib/tanStack/queryClient';
 
 import SessionWrapper from './SessionWrapper';
 
 export function AppProviders({ children }: { children: ReactNode }) {
+  const queryClient = getQueryClient() 
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
